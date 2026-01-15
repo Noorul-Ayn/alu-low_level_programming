3742bfe5794fd66b27b748ca26246b033a9ed364
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-01-15 13:10:17 +0200

    Add program to print single digit numbers from 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints all single digit numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+
+	return (0);
+}
