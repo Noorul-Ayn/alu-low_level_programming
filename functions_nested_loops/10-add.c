a1c56127d2481b973b5169f88cdc5b114064a0c5
ADEODATUS NKUNDIMANA <a.nkundiman@alustudent.com>
2026-01-22 09:54:58 +0200

    Create 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Description: Returns the sum of a and b
+ *
+ * Return: The sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
